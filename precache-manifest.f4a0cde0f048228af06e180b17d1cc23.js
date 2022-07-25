41cbc192067de7eae06cd92b88ed5aeb13796f6b
ammaramame <ammar.amame@l-one.de>
2022-07-25 09:07:03 +0300

    init commit

---
+++ precache-manifest.f4a0cde0f048228af06e180b17d1cc23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6d2d8fe64da81d8597",
+    "url": "/css/chunk-vendors.3d711198.css"
+  },
+  {
+    "revision": "7f44706906431fa7eaf9",
+    "url": "/css/companies.e851b409.css"
+  },
+  {
+    "revision": "826b95c582bd6a780ab2c499c548435e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1828b6f3f651281a14",
+    "url": "/js/app.917968ff.js"
+  },
+  {
+    "revision": "cb6d2d8fe64da81d8597",
+    "url": "/js/chunk-vendors.8065bc28.js"
+  },
+  {
+    "revision": "7f44706906431fa7eaf9",
+    "url": "/js/companies.87046605.js"
+  },
+  {
+    "revision": "5b5284d2685f4e63692be43de565edd8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
